9b6d3bd06d89c35d2ae59e7698b1451a45b8adcc
Yaroslav Vyshynskyi <92641328+YaroslavVyshynskyi@users.noreply.github.com>
2022-06-01 21:21:56 +0300

    add css gallery

--- src/index.js
+++ src/index.js
@@ -1,31 +1,6 @@
 import Notiflix from "notiflix";
 import PixabayService from "./js/pixabay-api-service";
 
-// const articlesTpl = `
-// <div class="photo-card">
-//   <img src="${largeImageURL}" alt="${previewURL}" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>likes:</b> 
-//       ${likes}
-//     </p>
-//     <p class="info-item">
-//       <b>views:</b> 
-//       ${views}
-//     </p>
-//     <p class="info-item">
-//       <b>comments:</b>
-//       ${comments}
-//     </p>
-//     <p class="info-item">
-//       <b>downloads</b>
-//       ${downloads}
-//     </p>
-//   </div>
-// </div>
-// `;
-
-
 const form = document.querySelector("#search-form");
 const galleryContainer = document.querySelector(".gallery");
 const pixabayService = new PixabayService();
